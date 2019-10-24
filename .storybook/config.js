ffe322fe73a31937e07ac764a5389949b9d47147
Jeff-M-Hsu <jeff_m_hsu@hotmail.com>
2019-10-23 21:57:10 -0700

    Jest Integration
    Added Jest to the dev environment for visual regression testing. Jest automatically takes a snapshot of every story as soon as it is written.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
 import GlobalStyle from '../src/lib/themes/GlobalStyle';
+import requireContext from 'require-context.macro'; //workaround for Jest to recognize Webpack's require.context function
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src/lib', true, /stories\.js$/), module);
+configure(requireContext('../src/lib', true, /stories\.js$/), module);
 
 addDecorator(s => <><GlobalStyle />{s()}</>);
