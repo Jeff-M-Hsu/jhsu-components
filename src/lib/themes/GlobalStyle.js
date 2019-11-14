7ca1222b3eede1ed0c1ba828c0a7b8de9d1a3469
jhsu42 <jhsu42@uwo.ca>
2019-11-13 20:51:22 -0800

    Import Robot font

--- src/lib/themes/GlobalStyle.js
+++ src/lib/themes/GlobalStyle.js
@@ -1,9 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+	@font-face{
+		font-family: "Roboto";
+		src: url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+	}
   * {
-    @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
-    font-family: Roboto, Helvetica, sans-serif !important;
+    font-family: "Roboto", Helvetica, sans-serif !important;
   }
 `
 
